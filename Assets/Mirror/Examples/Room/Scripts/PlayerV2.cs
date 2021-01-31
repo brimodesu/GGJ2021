e0366d3755f2f3d1a148dc6e25ac026b5a937143
David Carrera <brim.carrera@gmail.com>
2021-01-31 11:59:30 -0600

    new player script

---
+++ Assets/Mirror/Examples/Room/Scripts/PlayerV2.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Mirror.Examples.Tanks
+{
+    public enum Skin
+    {
+        Dogger,
+        Tiburoncin,
+        Rhino,
+        Wolfang
+    }
+
+    public class PlayerV2 : NetworkBehaviour
+    {
+        [Header("Components")]
+        public NavMeshAgent agent;
+        public Animator animator;
+
+        [Header("Movement")]
+        public float rotationSpeed = 100;
+
+        [Header("Firing")]
+        public KeyCode shootKey = KeyCode.Space;
+        public GameObject projectilePrefab;
+        public Transform projectileMount;
+
+        [Header("Skin")]
+        public List<GameObject> skins = new List<GameObject>();
+        public Transform skin_parent;
+        
+        [SyncVar(hook = nameof(OnSkinChanged))] 
+        public Skin selectedSkin;
+
+        public override void OnStartLocalPlayer() {
+            CmdSetupPlayer((Skin)RandomEnumValue<Skin>());
+        }
+
+        void Update()
+        {
+            // movement for local player
+            if (!isLocalPlayer) return;
+
+            // rotate
+            float horizontal = Input.GetAxis("Horizontal");
+            transform.Rotate(0, horizontal * rotationSpeed * Time.deltaTime, 0);
+
+            // move
+            float vertical = Input.GetAxis("Vertical");
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            agent.velocity = forward * vertical * agent.speed;
+            animator.SetBool("Moving", agent.velocity != Vector3.zero);
+
+            // shoot
+            if (Input.GetKeyDown(shootKey))
+            {
+                CmdFire();
+            }
+        }
+
+        void OnSkinChanged(Skin _Old, Skin _New) {
+            foreach (var skin in skins)
+            {
+                if (skin.name.Equals(selectedSkin.ToString()))
+                {
+                    skin.SetActive(true);
+                }
+            }
+        }
+
+        // this is called on the server
+        [Command]
+        void CmdFire()
+        {
+            GameObject projectile = Instantiate(projectilePrefab, projectileMount.position, transform.rotation);
+            NetworkServer.Spawn(projectile);
+            RpcOnFire();
+        }
+
+        // this is called on the tank that fired for all observers
+        [ClientRpc]
+        void RpcOnFire()
+        {
+            animator.SetTrigger("Shoot");
+        }
+
+        [Command]
+        public void CmdSetupPlayer(Skin skin) {
+            selectedSkin = skin;
+        }
+
+        public static T RandomEnumValue<T>()
+        {
+            var values = Enum.GetValues(typeof(T));
+            int random = UnityEngine.Random.Range(0, values.Length);
+            return (T)values.GetValue(random);
+        }
+    }
+}
